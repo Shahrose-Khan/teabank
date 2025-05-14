3c5648ec35a3bb0240165dc42d766c0470f827a2
Shahrose Khan <shezee247@outlook.com>
2025-05-14 18:44:53 +0500

    Update: DB is updated to MongoDBAtlas

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+USERNAME = 'shahrosekhan1362'
+PASSEORD = '6Szs8M847vxWrx6p'
+# MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_SERVER = 'mongodb+srv://shahrosekhan1362:6Szs8M847vxWrx6p@dbserver.2etzm1e.mongodb.net/?retryWrites=true&w=majority&appName=DBServer'
 
-client = AsyncIOMotorClient(MONGO_DETAILS)
+client = AsyncIOMotorClient(MONGO_SERVER)
 database = client["teapot"]
 
 def get_database():
